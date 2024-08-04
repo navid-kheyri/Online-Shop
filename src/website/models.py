2a76be807f8bfca55785b8ea84ab1f1f5a1bf410
navid <navid.kheyri@yahoo.com>
2024-08-04 15:07:38 +0330

    Category model implemented

--- src/website/models.py
+++ src/website/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField()
+    image = models.ImageField(upload_to='category/%Y/%m/%d/',blank=True, null=True,default='default.jpg')
+    parent=models.ForeignKey('self',null=True, blank=True, on_delete=models.DO_NOTHING, related_name='sub_category')
