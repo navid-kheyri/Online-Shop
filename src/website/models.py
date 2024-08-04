89ed571e18eb5370950c51b8c8a20cf1421e1042
navid <navid.kheyri@yahoo.com>
2024-08-04 20:20:59 +0330

    Vendor model implemented

--- src/website/models.py
+++ src/website/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MaxLengthValidator, MinLengthValidator
 from django_jalali.db import models as jmodels
 
 # Create your models here.
@@ -7,23 +8,40 @@ from django_jalali.db import models as jmodels
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     description = models.TextField()
-    image = models.ImageField(upload_to='category/%Y/%m/%d/',blank=True, null=True,default='default.jpg')
-    parent=models.ForeignKey('self',null=True, blank=True, on_delete=models.DO_NOTHING, related_name='sub_categories')
+    image = models.ImageField(
+        upload_to='category/%Y/%m/%d/', blank=True, null=True, default='default.jpg')
+    parent = models.ForeignKey('self', null=True, blank=True,
+                               on_delete=models.DO_NOTHING, related_name='sub_categories')
+
 
 class Product(models.Model):
-    name= models.CharField(max_length=100)
-    quantity_in_stock=models.IntegerField()
+    name = models.CharField(max_length=100)
+    quantity_in_stock = models.IntegerField()
     description = models.TextField()
-    price=models.DecimalField(decimal_places=2)
-    discount=models.DecimalField(decimal_places=0)
-    created_at=jmodels.jDateTimeField(auto_now_add=True)
-    updated_at=jmodels.jDateTimeField(auto_now=True)
-    category=models.ForeignKey(Category,on_delete=models.CASCADE,related_name="products")
-    vendor=models.ForeignKey('Vendor',on_delete=models.CASCADE,related_name='products')
-    
+    price = models.DecimalField(decimal_places=2)
+    discount = models.DecimalField(decimal_places=0)
+    created_at = jmodels.jDateTimeField(auto_now_add=True)
+    updated_at = jmodels.jDateTimeField(auto_now=True)
+    category = models.ForeignKey(
+        Category, on_delete=models.CASCADE, related_name="products")
+    vendor = models.ForeignKey(
+        'Vendor', on_delete=models.CASCADE, related_name='products')
+
+
 class ProductImage(models.Model):
-    title=models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
     description = models.TextField()
-    image=models.ImageField(upload_to='product/%Y/%m/%d/')
-    created_at=jmodels.jDateTimeField(auto_now_add=True)
-    Product=models.ForeignKey(Product,on_delete=models.DO_NOTHING,related_name="images")
+    image = models.ImageField(upload_to='product/%Y/%m/%d/')
+    created_at = jmodels.jDateTimeField(auto_now_add=True)
+    Product = models.ForeignKey(
+        Product, on_delete=models.DO_NOTHING, related_name="images")
+
+
+class Vendor(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    phone = models.CharField(
+        validators=[MinLengthValidator(11), MaxLengthValidator(11)])
+    email = models.EmailField()
+    status = models.BooleanField()
+    created_at = jmodels.jDateTimeField(auto_now_add=True)
+    address = models.OneToOneField('Address', on_delete=models.PROTECT)
