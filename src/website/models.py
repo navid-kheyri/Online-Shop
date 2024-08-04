f5170143f1014762d17e0ae8da9aea403ea08528
navid <navid.kheyri@yahoo.com>
2024-08-04 20:34:44 +0330

    VendorImage model implemented

--- src/website/models.py
+++ src/website/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-from django.core.validators import MaxLengthValidator, MinLengthValidator
+from django.core.validators import (MaxLengthValidator, MinLengthValidator,
+                                    MaxValueValidator,MinValueValidator)
 from django_jalali.db import models as jmodels
 
 # Create your models here.
@@ -45,3 +46,12 @@ class Vendor(models.Model):
     status = models.BooleanField()
     created_at = jmodels.jDateTimeField(auto_now_add=True)
     address = models.OneToOneField('Address', on_delete=models.PROTECT)
+
+
+class VendorImage(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    image = models.ImageField(upload_to='product/%Y/%m/%d/')
+    created_at = jmodels.jDateTimeField(auto_now_add=True)
+    vendor = models.ForeignKey(
+        Vendor, on_delete=models.DO_NOTHING, related_name="images")
