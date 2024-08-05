0742aa18548313b1d913644577a3115eab0e9cc0
navid <navid.kheyri@yahoo.com>
2024-08-05 15:34:02 +0330

    Vendor amdin.py implemented

--- src/vendors/admin.py
+++ src/vendors/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import Vendor, VendorImage
 # Register your models here.
+
+
+@admin.register(Vendor)
+class VendorModelAdmin(admin.ModelAdmin):
+    list_display = ['name', 'phone', 'email',
+                    'status', 'created_at', 'address']
+    ordering = ['status']
