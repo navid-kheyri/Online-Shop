2e1e648291c03eaee4b6260e6d2a861633900384
navid <navid.kheyri@yahoo.com>
2024-08-05 15:18:28 +0330

    Cart admin.py implemented

--- src/orders/admin.py
+++ src/orders/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Cart, OrderItem
 # Register your models here.
+
+
+@admin.register(Cart)
+class CartModelAdmin(admin.ModelAdmin):
+    list_display = ['status', 'crated_at', 'total_price']
+    ordering = ['status']
+
+    def total_price(self, instance):
+        return instance.get_total_amount()
