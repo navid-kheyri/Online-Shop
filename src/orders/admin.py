1b1aa9ea6843962495dc96214585b314c6ff2fbd
navid <navid.kheyri@yahoo.com>
2024-08-05 15:24:22 +0330

    OrderItem admin.py implemented

--- src/orders/admin.py
+++ src/orders/admin.py
@@ -5,8 +5,16 @@ from .models import Cart, OrderItem
 
 @admin.register(Cart)
 class CartModelAdmin(admin.ModelAdmin):
-    list_display = ['status', 'crated_at', 'total_price']
-    ordering = ['status']
+    list_display = ['status', 'created_at', 'total_price']
+    ordering = ['status','created_at']
 
     def total_price(self, instance):
         return instance.get_total_amount()
+
+
+@admin.register(OrderItem)
+class OrderItemModelAdmin(admin.ModelAdmin):
+    list_display=['number','cart','product','quantity','item_total_price']
+
+    def number(self,instance):
+        return instance.id
