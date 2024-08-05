ee91ce4d65bc8aa8650ba81832c951eece14386d
navid <navid.kheyri@yahoo.com>
2024-08-05 13:05:14 +0330

    Address model implemented

--- src/customers/models.py
+++ src/customers/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.core.validators import (MinLengthValidator, MaxLengthValidator)
 
 # Create your models here.
+
+
+class Address(models.Model):
+    state = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    street = models.CharField(max_length=50)
+    zipcode = models.CharField(max_length=10, blank=True,
+                               validators=[
+                                   MinLengthValidator(10),
+                                   MaxLengthValidator(10)
+                               ])
+
+    def __str__(self):
+        return f"{self.id} {self.state}, {self.city}, {self.street}"
