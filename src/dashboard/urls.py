f85ffd20cd8d2ffea700da305ab436de97250069
navid <navid.kheyri@yahoo.com>
2024-08-07 15:29:16 +0330

    customer dashboard detailview implemented

--- src/dashboard/urls.py
+++ src/dashboard/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-
+from .views import CustomerDetailView
 app_name='dashboard'
 
 urlpatterns = [
-    path('customer/<pk>/detailes/',)
+    path('customer/<pk>/detailes/',CustomerDetailView.as_view(),name='user')
 ]
--- src/dashboard/views.py
+++ src/dashboard/views.py
@@ -1,3 +1,21 @@
+from typing import Any
 from django.shortcuts import render
+from django.views.generic import DetailView
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
 
 # Create your views here.
+
+class CustomerDetailView (DetailView):
+    model=User
+    template_name='dashboard/dashboard.html'
+
+    def get_context_data(self, **kwargs) :
+        context= super().get_context_data(**kwargs)
+        user_id=self.object
+        user=User.objects.get(id=user_id)
+        context['user']=user
+        return context
