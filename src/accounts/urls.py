79c02722dc77f9a7f267db707d0fa2a3b1d6c522
navid <navid.kheyri@yahoo.com>
2024-08-07 15:31:54 +0330

    LoginView implemented

--- src/accounts/urls.py
+++ src/accounts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import login_user
+from .views import CustomLoginView
 
 app_name='accounts'
 
 urlpatterns = [
-    path('login/',login_user,name='login')
+    path('login/',CustomLoginView.as_view(),name='login'),
 ]
--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.contrib.auth import authenticate, login, logout
 
-def login_user(request):
-    return render(request,'accounts/login.html')
+# Create your views here.
+
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('registerr:test')
+
+    def form_valid(self, form):
+        login(self.request, form.get_user())
+        return super(CustomLoginView, self).form_valid(form)
