7b5bffac69da339f8adca1dd99f53d41ee1d0376
navid <navid.kheyri@yahoo.com>
2024-08-21 00:19:43 +0330

    OrderDetailAPIView implemented

--- src/orders/api/v1/views.py
+++ src/orders/api/v1/views.py
@@ -24,3 +24,31 @@ class OrderListCreateAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class OrderDetailAPIView(APIView):
+    def get(self, request, pk, *args, **kwargs):
+        try:
+            order = Order.objects.get(pk=pk)
+        except Order.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = OrderModelSerializer(order)
+        return Response(serializer.data)
+
+    def put(self, request, pk, *args, **kwargs):
+        try:
+            order = Order.objects.get(pk=pk)
+        except Order.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = OrderModelSerializer(order, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, *args, **kwargs):
+        try:
+            order = Order.objects.get(pk=pk)
+        except Order.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        order.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
