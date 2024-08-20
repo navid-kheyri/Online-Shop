21f67e5c900a1eaef9070256738b708d9c56d734
navid <navid.kheyri@yahoo.com>
2024-08-21 00:18:49 +0330

    OrderListCreateAPIView implemented

--- src/orders/api/v1/serializers.py
+++ src/orders/api/v1/serializers.py
@@ -1,20 +1,21 @@
 from rest_framework import serializers
-from ...models import Order,OrderItem
+from ...models import Order, OrderItem
 from customers.models import Address
 
 
 class OrderItemModelSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderItem
-        fields='__all__'
+        fields = '__all__'
 
 
 class OrderModelSerializer(serializers.ModelSerializer):
     order_items = OrderItemModelSerializer(many=True, read_only=True)
-    
+
     class Meta:
-        model=Order
-        fields='__all__'
+        model = Order
+        fields = '__all__'
+
 
 class AddressSerializer(serializers.ModelSerializer):
     class Meta:
@@ -22,10 +23,10 @@ class AddressSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-
 class CartAddSerializer(serializers.Serializer):
     product_id = serializers.IntegerField()
     quantity = serializers.IntegerField(default=1)
+
 
 class CartRemoveSerializer(serializers.Serializer):
     product_id = serializers.IntegerField()
---
+++ src/orders/api/v1/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import get_object_or_404
+from django.http import JsonResponse
+from rest_framework.permissions import IsAdminUser
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from ...models import Order, OrderItem
+from .serializers import OrderItemModelSerializer,OrderModelSerializer
+from website.models import Product
+from ...cart import Cart
+
+from customers.models import Address
+from .serializers import CartAddSerializer, CartRemoveSerializer,AddressSerializer
+
+class OrderListCreateAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        orders = Order.objects.all()
+        serializer = OrderModelSerializer(orders, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer = OrderModelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
