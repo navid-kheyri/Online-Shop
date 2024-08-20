bfd12dfa947e039296ecbf9428585b30e5627fea
navid <navid.kheyri@yahoo.com>
2024-08-21 00:20:44 +0330

    OrderItemDetailAPIView implemented

--- src/orders/api/v1/views.py
+++ src/orders/api/v1/views.py
@@ -66,3 +66,32 @@ class OrderItemListCreateAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+
+class OrderItemDetailAPIView(APIView):
+    def get(self, request, pk, *args, **kwargs):
+        try:
+            order_item = OrderItem.objects.get(pk=pk)
+        except OrderItem.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = OrderItemModelSerializer(order_item)
+        return Response(serializer.data)
+
+    def put(self, request, pk, *args, **kwargs):
+        try:
+            order_item = OrderItem.objects.get(pk=pk)
+        except OrderItem.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        serializer = OrderItemModelSerializer(order_item, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, *args, **kwargs):
+        try:
+            order_item = OrderItem.objects.get(pk=pk)
+        except OrderItem.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        order_item.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
