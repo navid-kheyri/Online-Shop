8e650c7dfa27d76a2f52c745face26c44dc45664
navid <navid.kheyri@yahoo.com>
2024-08-21 00:20:10 +0330

    OrderItemListCreateAPIView implemented

--- src/orders/api/v1/views.py
+++ src/orders/api/v1/views.py
@@ -52,3 +52,17 @@ class OrderDetailAPIView(APIView):
             return Response(status=status.HTTP_404_NOT_FOUND)
         order.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    
+
+class OrderItemListCreateAPIView(APIView):
+    def get(self, request, *args, **kwargs):
+        order_items = OrderItem.objects.all()
+        serializer = OrderItemModelSerializer(order_items, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        serializer = OrderItemModelSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
