3c8bd10d64004169b8ee7bbd3165d74829b3163d
navid <navid.kheyri@yahoo.com>
2024-09-02 12:47:47 +0330

    TestVendorModel and TestVendorImage implemented

--- src/orders/tests/test_orders_models.py
+++ src/orders/tests/test_orders_models.py
@@ -4,7 +4,7 @@ from website.models import Category
 from vendors.models import Vendor
 
 
-class OrderTestCase(TestCase):
+class TestOrderModel(TestCase):
     def test_create_order(self):
         user_obj = User.objects.create(email='navid@gmail.com', phone_number='09121234567',
                                        age=30, city='tehran', user_type='customer')
@@ -17,7 +17,7 @@ class OrderTestCase(TestCase):
         self.assertEqual(order.address, addr_obj)
 
 
-class OrderItemTestCase(TestCase):
+class TestOrderItemModel(TestCase):
     def test_create_orderitem(self):
         user_obj = User.objects.create(email='navid@gmail.com', phone_number='09121234567',
                                        age=30, city='tehran', user_type='customer')
@@ -35,7 +35,9 @@ class OrderItemTestCase(TestCase):
         product_obj.vendor.add(vendor_obj)
         orderitem = OrderItem.objects.create(quantity=10, item_total_price=1000.00, product=product_obj,
                                              order=order_obj)
-
+        
+        self.assertEqual(vendor_obj.user.get(pk=user_obj.pk) , user_obj)
+        self.assertEqual(product_obj.vendor.get(pk=vendor_obj.pk) , vendor_obj)
         self.assertEqual(orderitem.quantity, 10)
         self.assertEqual(orderitem.item_total_price, 1000.00)
         self.assertEqual(orderitem.product, product_obj)
---
+++ src/vendors/tests/test_vendors_models.py
@@ -0,0 +1,36 @@
+from django.test import TestCase, Client
+from ..models import Vendor, VendorImage, VendorRating, User
+
+
+class TestVendorModel(TestCase):
+    def test_create_vendor(self):
+        user_obj = User.objects.create(email='navid@gmail.com', phone_number='09121234567',
+                                       age=30, city='tehran', user_type='customer')
+
+        vendor = Vendor.objects.create(name='test vendor', phone='02188888888', email='test@gmail.com',
+                                       status=True, address='azadi meydoon')
+        vendor.user.add(user_obj)
+
+        self.assertEqual(vendor.user.get(pk=user_obj.pk), user_obj)
+        self.assertEqual(vendor.name, 'test vendor')
+        self.assertEqual(vendor.phone, '02188888888')
+        self.assertEqual(vendor.email, 'test@gmail.com')
+        self.assertTrue(vendor.status)
+        self.assertEqual(vendor.address, 'azadi meydoon')
+
+
+class TestVendorImage(TestCase):
+    def test_create_vendor_iamge(self):
+        user_obj = User.objects.create(email='navid@gmail.com', phone_number='09121234567',
+                                       age=30, city='tehran', user_type='customer')
+
+        vendor_obj = Vendor.objects.create(name='test vendor', phone='02188888888', email='test@gmail.com',
+                                           status=True, address='azadi meydoon')
+        vendor_obj.user.add(user_obj)
+
+        vendor_image = VendorImage.objects.create(
+            title='test', image='media/default.jpg', vendor=vendor_obj)
+
+        self.assertEqual(vendor_obj.user.get(pk=user_obj.pk), user_obj)
+        self.assertEqual(vendor_image.title, 'test')
+        self.assertEqual(vendor_image.image, 'media/default.jpg')
